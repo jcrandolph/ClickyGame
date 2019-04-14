05bdd4b168bf4b35af1ea831beda16c6be7445d4
jcrandolph <jackcrandi@gmail.com>
2019-04-14 00:02:02 -0400

    Updates

---
+++ precache-manifest.c6790b6b62d7603cf155d99bf287bffe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "cfee8875284b5a7c480b",
+    "url": "/ClickyGame/static/js/main.b2a0a0a2.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "cfee8875284b5a7c480b",
+    "url": "/ClickyGame/static/css/main.699f62ed.chunk.css"
+  },
+  {
+    "revision": "335a027c0f07724b4fcb445770564ce2",
+    "url": "/ClickyGame/index.html"
+  }
+];
